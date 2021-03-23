123e55ff7af4104a45b2702a04f66a0e3822c343
lvidal-gs <lucasvidal.gs@gmail.com>
2021-03-23 18:19:46 -0300

    Adição de tela de login e de cadastro

--- client/src/components/EstruturaLogin.jsx
+++ client/src/components/EstruturaLogin.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
-import Logo from '../images/logo.png'
+import React from 'react';
+import Logo from '../images/logo.png';
+import '../global.css'
+
+import { Link } from 'react-router-dom';
 
 export default (props) =>
-    <div className="login-fields">
+    <div className="fields-area">
         <div>
             <img id="logo-menu" src={Logo} />
         </div>
@@ -17,11 +20,12 @@ export default (props) =>
             <input id="password" type="password" maxLength="30" placeholder="Insira sua senha" />
         </div>
 
-        <button id="button-login" type="submit">Login</button>
+        <button className="button-accept" type="submit">Login</button>
 
         <div className="footer">
-            <a href="#">Não possuo cadastro</a>
+            <Link to="/RegisterScreen"> Não possuo cadastro </Link>
             <a href="#">Esqueci minha senha</a>
         </div>
-
     </div>
+
+
--- client/src/pages/Login/index.jsx
+++ client/src/pages/Login/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './index.css';
+
 import Login from '../../components/EstruturaLogin'
 
 const Main = () => {
     return (
         <React.Fragment>
-            <div id="homepage">
-                <Login/>
+            <div className="toFill-area">
+                <div id="background-login">
+                    <Login />
+                </div>
             </div>
         </React.Fragment>
     )
