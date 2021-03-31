72f1a662f42c03f4eb85b62270e1eb8568cae3b3
Marcos S <py.analysis@gmail.com>
2021-03-31 18:30:22 -0300

    LeaderBoard Page

--- client/src/routes.js
+++ client/src/routes.js
@@ -8,12 +8,14 @@ import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Login from "./pages/Login";
 import RegisterScreen from "./pages/RegisterScreen";
 import SocketTest from "./pages/SocketTest";
+import LeaderBoard from "./pages/LeaderBoard";
 
 const Routes = () => (
     <BrowserRouter>
         <Switch>
             <Route exact path="/" component={ Login }/>
             <Route path="/socket" component= { SocketTest } />
+            <Route path="/ranking" component= { LeaderBoard } />
             <Route exact path="/RegisterScreen" component={ RegisterScreen }>
                 <RegisterScreen/>
             </Route>
