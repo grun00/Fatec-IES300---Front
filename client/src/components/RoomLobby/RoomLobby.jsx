c1e749afe50b467e3c6775e3bcf126233aba12e7
lvidal-gs <lucasvidal.gs@gmail.com>
2021-05-15 15:59:07 -0300

    Lobby use states added

--- client/src/components/RoomLobby/RoomLobby.jsx
+++ client/src/components/RoomLobby/RoomLobby.jsx
@@ -4,14 +4,16 @@ import AvatarIcon from "../../assets/avatar.svg";
 
 const RoomLobby = (props) => {
   const [numberUser, setNumberUser] = useState("0/2");
+  const [roomName, setRoomName] = useState("Sala X");
+  const [roomCode, setRoomCode] = useState("0001");
 
   return (
     <div id="Lobby-field">
       <div id="room-info">
         <img id="icon-field" src={AvatarIcon} alt="Avatar Icon" />
         <div id="room-name-code">
-          <span id="room-name">Sala 1</span>
-          <span id="room-code">#0000</span>
+          <span id="room-name">{roomName}</span>
+          <span id="room-code">#{roomCode}</span>
         </div>
       </div>
       <div id="infos-field">
